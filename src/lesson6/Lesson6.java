f7bbb632c498e57a0759058fd8a852c22c0b1cfd
NastyBear <lyalechka_24@mail.ru>
2022-10-19 09:46:33 +0300

    javaCore.lesson3

---
+++ src/lesson6/Lesson6.java
@@ -0,0 +1,38 @@
+package lesson6;
+
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+
+public class Lesson6 {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host("api.weather.yandex.ru")
+                .addPathSegment("v2")
+                .addPathSegment("informers")
+                .addQueryParameter("lat","59.56")
+                .addQueryParameter("lon","30.18")
+                .addQueryParameter("limit","5")
+
+                .build();
+
+        Request request = new Request.Builder()
+                .addHeader("X-Yandex-API-Key","a6eaa128-562d-4498-83e7-ecd3cbaf8b2a")
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+
+      String  jsonResponse = client.newCall(request).execute().body().string();
+        System.out.println(response.code());
+        System.out.println(jsonResponse);
+
+    }
+}
